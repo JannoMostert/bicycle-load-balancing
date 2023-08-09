f47b7d287ff10575c84a5d9bb3468f5f4f4548c0
JannoMostert <57058488+JannoMostert@users.noreply.github.com>
2023-08-09 14:15:24 +0200

    investigate-data-structure

--- bicycle_station_analysis.sql
+++ bicycle_station_analysis.sql
@@ -1,3 +1,11 @@
-select *
-from `bigquery-public-data.london_bicycles.cycle_stations` 
-limit 10
+SELECT
+  CAST(DATE_TRUNC(start_date, month) AS DATE) AS rental_month,
+  start_station_name,
+  COUNT(DISTINCT rental_id)     AS rental_count
+FROM
+  `bigquery-public-data.london_bicycles.cycle_hire` 
+WHERE CAST(start_date AS DATE) >= CAST('2022-01-01' AS DATE)
+GROUP BY
+  1, 2
+ORDER BY rental_month DESC
+;
